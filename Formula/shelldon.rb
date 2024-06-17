ad2b06f86fd0546eaf9e20cbdbae8a5c2caedc59
Douglas Makey Mendez Molero <douglasmakey@gmail.com>
2024-06-16 20:53:34 -0400

    Bump version from v0.1.0 to v0.1.1

--- Formula/shelldon.rb
+++ Formula/shelldon.rb
@@ -1,8 +1,8 @@
 class Shelldon < Formula
   desc "Shelldon is your new Rust-powered buddy with GPT features!"
   homepage "https://github.com/douglasmakey/shelldon"
-  url "https://github.com/douglasmakey/shelldon/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "00b4dd2db5ea4543ca52a5ac1d9b06aa33ca06cc93208c7a626e78c4eb30d5d6"
+  url "https://github.com/douglasmakey/shelldon/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "5fd2b7609fb095db4e885434b379de464c760f0e2907d60e342e0ccbd771ad7c"
   license "MIT"
 
   depends_on "rust" => :build
